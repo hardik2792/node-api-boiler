e17daebe8943e83a1bfe545705ad8f30fbb3450c
hardik2792 <hardikthakor2792@gmail.com>
2019-06-02 15:15:09 +0530

    Dependency Updated

---
+++ server/connectors/socket.js
@@ -0,0 +1,16 @@
+module.exports = (server) => {
+  const io = require('socket.io')(server);
+  io.on('connection', function(client) {
+      console.log("Connected",client.id);
+      client.on('disconnect', function() {
+        console.log("disconnected");
+      });
+      client.on('room', function(data) {
+        client.join(data.roomId);
+        console.log(' Client joined the room and client id is '+ client.id);
+      });
+      client.on('toBackEnd', function(data) {
+        client.in(data.roomId).emit('message', data);
+      });
+  });
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,15 +3,15 @@ File: index.js
 App Name: API Demo
 Purpose: Routing For API Call
 Created By: Hardik Thakor */
-// const Router 	= require('express')();
+
 const mongoose  = require('mongoose');
 mongoose.set('useFindAndModify', false);
 const chalk 	= require('chalk');
 const dbControls= require('../config/appConfig').db;
 
-const control = require('../controllers/controller');
-const foCtrls = require('../controllers/fileOperations');
-const cmdCtrls = require('../controllers/cmdController');
+const control 	= require('../controllers/controller');
+const foCtrls 	= require('../controllers/fileOperations');
+const cmdCtrls 	= require('../controllers/cmdController');
 
 // Connecting to Database
 const dbConnectURL = dbControls.url;
@@ -20,13 +20,11 @@ const dbOptions = {
                     keepAlive: 300000,
                     poolSize: 2,
                     promiseLibrary: global.Promise,
-                  };
+                };
 mongoose.Promise = global.Promise;
-mongoose.connect(dbConnectURL,dbOptions)
-    .then(() => {
+mongoose.connect(dbConnectURL,dbOptions).then(() => {
         console.log(chalk.blue('Connection to Database is '),chalk.green.bold("Successful!"));
-    })
-    .catch((error) => {
+    }) .catch((error) => {
         console.log(chalk.red('Connection to Database '),chalk.red.bold("Failed!"),chalk.red('\n Due to' ,error));
     });
 
@@ -36,8 +34,8 @@ module.exports=function(app) {
 		res.send({'success':true,'message':'Successfully Connected!'})
 	});
 
-	app.get("/gettodo", control.gettodo);
 	app.post("/addtodo", control.addtodo);
+	app.get("/gettodo", control.gettodo);
 	app.put("/updatetodo", control.updatetodo);
 	app.delete("/deletetodo/:id", control.deletetodo);
 
